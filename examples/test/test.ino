a6d3fad7e8aa2e664bafafaf6b83a539066b941f
max30012540 <max30012540@hotmail.com>
2018-05-19 19:22:23 +0700

    Fix to work

--- examples/test/test.ino
+++ examples/test/test.ino
@@ -1,16 +1,21 @@
+/* File              : test.ino
+   Codeing By IOXhop : www.ioxhop.com
+   Sonthaya Nongnuch : www.fb.me/maxthai */
+
 #include<IOXhop_BC95.h>
 
 IOXhop_BC95 nb;
 Socket *soc = NULL;
 
-unsigned long timeSend = 0;
+unsigned long timeSend = 0, timeSend2 = 0;
 
 void setup() {
   Serial.begin(9600);
 
   // Setup module
   nb.easySetup(true);
-  
+
+  // Socket
   Serial.println("Create new socket");
   if (soc = nb.CreateUDPSocket(9687)) {
     Serial.print("Socket id is ");
@@ -20,26 +25,27 @@ void setup() {
   }
   
   soc->onReceiver([](String ip, unsigned long port, byte* data, int len) {
-    data[len] = 0;
+    char message[len + 1];
+    memcpy(message, data, len);
+    message[len] = 0;
     Serial.print("Receiver[" + ip + ":" + String(port) + "]: ");
-    Serial.println((char*)data);
+    Serial.println(message);
   });
 }
 
 void loop() {
   if (soc) {
-    if ((millis() - timeSend) >= 1000) { // every 1 sec
+    if ((millis() - timeSend) >= 5000) { // every 5 sec
       timeSend = millis();
       char text[] = "Hello, NB-IoT !";
       Serial.print("Send ");
-      if (soc->send("45.77.40.88", 55567, text, strlen(text))) {
+      if (soc->send("149.28.51.94", 55567, text, strlen(text))) {
         Serial.println("OK");
       } else {
         Serial.println("fail !");
       }
     }
-    soc->CheckReceiver();
   }
-  delay(1);
-  //nb.loop();
+  
+  nb.loop();
 }
